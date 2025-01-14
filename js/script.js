a0b18b3341640cb0b32009771bec511b20b4aff5
nicolamarino <nicolamarino527@gmail.com>
2025-01-14 17:46:11 +0100

    setup

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// scriviamo le costanti
+const numberList = document.getElementById("numbers-list");
+
+
+// Generiamo le cifre da indovinare
+
+
+// Settiamo il tempo in cui possiamo vedere le cifre
+// Facciamo comparire gli imput dove inserire i numeri
+// Inviamo i dati e resettiamo il gioco
