300f7559247b296d6e99d4f0561d931a0f34c7db
nicolamarino <nicolamarino527@gmail.com>
2025-01-15 19:35:11 +0100

    add time function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 // scriviamo le costanti
 const numberList = document.getElementById("numbers-list");
-
+const button = document.getElementById("button");
 
 // Generiamo le cifre da indovinare
+let randomNumbersList = [];
+    for (let i = 0; i < 5; i++) {
+        let randomNumbers = Math.floor(Math.random() * 50) + 1;
+        randomNumbersList.push(randomNumbers);
+    };
+    console.log(randomNumbersList);
+
+// Rendiamo le cifre visibili nell html
+numberList.append(randomNumbersList);
 
 
 // Settiamo il tempo in cui possiamo vedere le cifre
-// Facciamo comparire gli imput dove inserire i numeri
-// Inviamo i dati e resettiamo il gioco
+const clock = setTimeout(disappear, 2000);
+
+// scriviamo la funzione che fa scomparire i numeri
+function disappear() {
+    numberList.classList.add("d-none");
+    console.log("ecco qua");   
+};
+
+
+
